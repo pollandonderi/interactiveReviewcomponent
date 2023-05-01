dcf4d5d56ae5458d975faf002a5ffe06d8452f22
polland <pollandsabisa@gmail.com>
2023-05-01 22:39:22 +0300

    finishing on funcctionality

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { __values } from 'tslib';
 
 @Component({
   selector: 'app-card',
@@ -6,21 +7,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 
-export class CardComponent implements OnInit {
-  
+export class CardComponent implements OnInit {  
 maincontainer:HTMLElement = document.querySelector(".main-container")!
   thankyoucontainer = document.querySelector(".card2");
   public submitbutton = document.getElementById("submit");
+  public visible = "none";
+  public Invisible = "";
+  public number: number[] = [];
+
+
 
   ngOnInit(): void {
-    if (this.submitbutton) {
-      this.submitbutton.addEventListener("click", () =>{
-        console.log("here we come")
-        this.maincontainer.classList.remove("hidden");
-        this.maincontainer.style.display = "none";
-      });
-    }
+    // if (this.submitbutton) {
+    //   this.submitbutton.addEventListener("click", () =>{
+    //     console.log("here we come")
+    //     this.maincontainer.classList.remove("hidden");
+    //     this.maincontainer.style.display = "none";
+    //   });
+    // }
   }
-  
+
+  onclick() {
+   this.visible = "";
+   this.Invisible = "none";
+   
+  }
+  click1(){
+    this.number.push(1)
+
+  }
+  click2(){
+    this.number.push(2)
+  }
+  click3(){
+    this.number.push(3)
+  }
+  click4(){
+    this.number.push(4)
+  }
+  click5(){
+    this.number.push(5)
+  }
 
 }
+
+
